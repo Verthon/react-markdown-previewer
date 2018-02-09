00191ea00b1361e8b015b0824c98062acfae1a74
Verthon <krzysiekksordyl@gmail.com>
2018-02-09 19:48:31 +0100

    functionality is working, but output is not provided by Output Component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ import marked from 'marked';
 
 class App extends Component {
   state = {
-    value: 'test',
     markdown: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n', 
   };
 
   inputHandler = (e) =>{
     this.setState({markdown: e.target.value});
-    console.log(e.target.value);
+  };
+
+  rawMarkUp = (markup) =>{
+    marked.setOptions({
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: true,
+      smartLists: true,
+      smartypants: false
+    });
+
+    let rawhtml = marked(this.state.markdown, {sanitize: true});
+    return {
+      __html: rawhtml
+    }
   }
 
   render() {
@@ -21,8 +36,9 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">React Markdown Previewer</h1>
         </header>
-        <Input changed={this.inputHandler} value={this.state.markdown}/>
-        <Output data={this.state.markdown}/>
+        <Input changed={this.inputHandler} markdown={this.state.markdown}/>
+        <Output/>
+        <div dangerouslySetInnerHTML={{__html: marked(this.state.markdown)}}></div>
       </div>
     );
   }
